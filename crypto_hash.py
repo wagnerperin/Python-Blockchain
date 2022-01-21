bec8eda3728b2685e887e5097e339c5fd9b22d13
Wagner Perin <wagnerperin@gmail.com>
2022-01-21 13:05:06 -0300

    Implement hash functionality to block generation

--- crypto_hash.py
+++ crypto_hash.py
@@ -1,15 +1,18 @@
 import hashlib
 import json
 
-def crypto_hash(data):
+def crypto_hash(*args):
     """"
-    Return a sha-256 hash of the given data.
+    Return a sha-256 hash of the given arguments.
     """
-    stringfied_data = json.dumps(data)
-    return hashlib.sha256(stringfied_data.encode('utf-8')).hexdigest()
+    stringfied_args = sorted(map(lambda data: json.dumps(data), args))
+    joined_data = ''.join(stringfied_args)
+
+    return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
 def main():
-    print(f"crypto_hash([2]): {crypto_hash([2])}")
+    print(f"crypto_hash('one', 2, [3]): {crypto_hash('one', 2, [3])}")
+    print(f"crypto_hash(2, 'one', [3]): {crypto_hash(2, 'one', [3])}")
 
 if __name__ == "__main__":
     main();
